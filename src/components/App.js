9fcfe13bc9a05c858f10a0fafc716da2190ac0dd
Teresa <tenjerim@gmail.com>
2023-01-16 21:38:17 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "../components/Header"
+import About from "../components/About"
+import ArticleList from "../components/ArticleList"
 
 console.log(blogData);
 
